d1172128e72f3bf15279c0ad62e7525db3cfe733
leloucj <gothicj@163.com>
2017-05-21 23:05:05 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,3 +1,9 @@
 module.exports = function main(email, suffixes) {
   // Write your code here
+    var e=email;
+  var s=suffixes[0];
+  if(e.indexOf(s)>=0)
+    return true;
+  else  
+return false;
 };
